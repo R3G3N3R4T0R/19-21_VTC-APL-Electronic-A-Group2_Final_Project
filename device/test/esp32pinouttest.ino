30a70489de8955677d6c1700369e98c2c6c3c035
R3G3N3R4T0R <dicktsui2003@gmail.com>
2020-12-03 05:51:00 +0900

    Include a pinout test file for esp32
    Include a testing firmware in./device/test/, the file has not been compiled

---
+++ device/test/esp32pinouttest.ino
@@ -0,0 +1,27 @@
+#include "Arduino.h"
+#include <string.h>
+#include "EEPROM.h"
+#ifndef __AVR__
+    #include "BLEDevice.h"
+    #include "BLEUtils.h"
+    #include "BLEServer.h"
+#endif
+/********************
+ * DEVICE FIRMWARE SOURCE CODE FOR THE FINAL PROJECT OF 19-21 VTC APL EE
+ * THE SOURCE IS PUBLISHED WITH THE MIT LICENSE ON GITHUB
+ * THE CODE WILL NOT BE MAINTAINED AFTER SUBMISSION
+ * PLEASE CHECK THE DOCUMENTATION FOR NECCESARY DEPENDENCIES
+ ********************/
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(1000);
+}
+
+void loop()
+{
+    for(pin = 16; pin <= 39; pin++)
+        if(digitalRead(pin) == HIGH)
+            Serial.println(pin);
+}
